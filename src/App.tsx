2a59339092f73e4e4bb423dd8d78872b25f7577a
sebastian <scsebastian1406@gmail.com>
2026-01-24 19:00:05 -0500

    Feat: Agregar formulario de recibos

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { FormularioRecibo } from './components/FormularioRecibo'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  function handleAgregar(monto: number, descripcion: string, pagado: boolean) {
+    console.log(monto, descripcion, pagado)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Gestor de recibos</h1>
+      <FormularioRecibo onAgregar={handleAgregar} />
+
     </>
   )
 }
